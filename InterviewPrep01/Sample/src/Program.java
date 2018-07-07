8918a5185295cec360f5f721f4f01ce1f1c9951d
Harini <Harini@DESKTOP-UUEPG1R.attlocal.net>
2018-07-07 10:55:57 -0700

    First commit

---
+++ InterviewPrep01/Sample/src/Program.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 List<Integer> list = new ArrayList<Integer>();
+	        list.add(10);
+	        list.add(10);
+	        
+	        System.out.print(list.size());
+	        list.remove(new Integer(10));
+	        System.out.print(list.size());
+	}
+
+}
